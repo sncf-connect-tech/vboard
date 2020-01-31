c458a4ebac7c9a9d21ff66d3b3ac85aa5fdeef08
Thomas Garesse <thomas.garesse@gmail.com>
2020-01-31 15:28:27 +0100

    Fix: overlapping pins visual glitch - close #87 (#90)
    Fix: overlapping pins visual glitch - close #87

--- vboard-front/src/main/common/vboardPinboard/scripts/vboardPinboard.js
+++ vboard-front/src/main/common/vboardPinboard/scripts/vboardPinboard.js
@@ -16,8 +16,6 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-angular.module('vboard').constant('VboardPinsContainerClassname', '.vboardPinboard');
-
 angular.module('vboard').directive('vboardPinboard', function vboardPinboard($timeout) {
     return {
         restrict: 'E',
@@ -33,31 +31,27 @@ angular.module('vboard').directive('vboardPinboard', function vboardPinboard($ti
                     columnWidth: 250,
                     gutter: 15
                 });
-                scope.packer.reloadItems();
             }
 
-            /** Recalculate the layout every time the directive is called */
-            $timeout(function () {
-                scope.packer.reloadItems();
-                scope.packer.layout();
-            }, 500);
-
-            scope.refresh = function () {
-                scope.packer.reloadItems();
-                scope.packer.layout();
+            scope.recomputeAfterImagesLoaded = function () {
+                // From the docs: "The 'always' event is triggered after all images have been either loaded or confirmed broken"
+                imagesLoaded('.vboardPinboard').on('always', function () {
+                    // Note: imagesLoaded does not correctly detect this,
+                    // and send this event a bit too early, so we add a slight delay
+                    // until we find a better fix / replacement for it.
+                    $timeout(() => {
+                        scope.packer.reloadItems();
+                        scope.packer.layout();
+                    }, 2000);
+                });
             };
+
+            scope.recomputeAfterImagesLoaded();
         }
     }
 });
 
-angular.module('vboard').controller('VboardPinboardController', function VboardPinboardController($rootScope, $scope, $q, $timeout, $element, waitUntil, vboardPinsCollection, VboardPinsContainerClassname) {
-
-    /** Display the layout after images finish loading */
-    const recomputeAfterImagesLoaded = function () {
-        imagesLoaded(VboardPinsContainerClassname, function () {
-            $scope.refresh();
-        });
-    };
+angular.module('vboard').controller('VboardPinboardController', function VboardPinboardController($rootScope, $scope, $q, $timeout, $element, waitUntil, vboardPinsCollection) {
 
     $rootScope.disableFavoriteLabels = true;
 
@@ -70,23 +64,18 @@ angular.module('vboard').controller('VboardPinboardController', function VboardP
     /** Packery Object for displaying pins */
     $scope.packer = false;
 
-    /** Display the pin panel */
-    $scope.render = function () {
-        return waitUntil.elemCreated(VboardPinsContainerClassname).then(function () {
-            // $scope.create = true; (Can be used to trigger function in the directive with a $watch)
-            return recomputeAfterImagesLoaded();
-        }).catch(function (error) {
-            throw error;
+    $scope.setPinsLikedAndSaved = function () {
+        vboardPinsCollection.getUserLikes().then(function (success) {
+            $scope.likes = success.data.map((like) => like.pin);
+        });
+        vboardPinsCollection.getUserSavedPins().then(function (success) {
+            $scope.saved = success.data.map((pin) => pin.pin_id);
         });
     };
 
     /** After the pins loading, the panel is re-displayed */
     $scope.$on('vboardPinsCollectionUpdated', function () {
-        $scope.render($scope.pinsCollection.pins);
-        $timeout(function () {
-            $scope.packer.reloadItems();
-            $scope.packer.layout();
-        }, 500);
+        $scope.recomputeAfterImagesLoaded();
     });
 
     /** Load other pins if scroll is down */
@@ -101,19 +90,7 @@ angular.module('vboard').controller('VboardPinboardController', function VboardP
 
     $rootScope.$watch('userAuthenticated', function () {
         if ($rootScope.userAuthenticated) {
-            /** Retrieve the user's likes */
-            vboardPinsCollection.getUserLikes().then(function (success) {
-                $scope.likes =[];
-                success.data.forEach(function (like) {
-                    // Put in scope.likes the pin's id liked by the current user
-                    $scope.likes.push(like.pin);
-                });
-            });
-            /** Retrieve the user's savedPins */
-            vboardPinsCollection.getUserSavedPins().then(function (success) {
-                $scope.saved = [];
-                $scope.saved = _.map(success.data, 'pin_id'); // Put in scope.saved the pin's id saved by the current user
-            });
+            $scope.setPinsLikedAndSaved();
         }
     });
 
