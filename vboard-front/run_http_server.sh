2ac5cd9b7505496be7057e673b4daacb43fb7505
Lucas Cimon <925560+Lucas-C@users.noreply.github.com>
2020-12-02 15:57:30 +0100

    Making HTTP port configurable in frontend

--- vboard-front/run_http_server.sh
+++ vboard-front/run_http_server.sh
@@ -2,7 +2,11 @@
 
 set -o pipefail -o errexit -o nounset -o xtrace
 
-: ${VBOARD_API_ENDPOINT?'Required env variable'}
+: ${VBOARD_WS_HOST?'Required env variable'}
+[ -z "${VBOARD_API_ENDPOINT:-}" ] && export VBOARD_HOSTNAME=/api/v1
+[ -z "${VBOARD_BLOG_URL:-}" ] && export VBOARD_BLOG_URL=
+[ -z "${VBOARD_HOSTNAME:-}" ] && export VBOARD_HOSTNAME=localhost
+[ -z "${PORT:-}" ] && export PORT=80
 
 cd /var/www/vboard
 
@@ -28,6 +32,10 @@ if [ -n "${KCK_REALM:-}" ] || [ -n "${KCK_PUBLIC_HOST:-}" ] || [ -n "${KEYCLOAK_
     echo 'Inserting $KEYCLOAK_JS_URL in index.html'
     sed -i "s~>window.Keycloak = 'DISABLED'~ src=\"/auth/js/keycloak.js\">~" index.html
 fi
+
+echo "Injecting \$PORT=$PORT in httpd.conf:"
+cp /usr/local/apache2/conf/httpd.conf /usr/local/apache2/conf/httpd.conf.bak
+envsubst '$PORT' < /usr/local/apache2/conf/httpd.conf.bak > /usr/local/apache2/conf/httpd.conf
 
 HTTPD_ARGS=
 echo "Launching Apache httpd in foreground: HTTP_PROXY=${HTTP_PROXY:-} KCK_PUBLIC_HOST=${KCK_PUBLIC_HOST:-}"
